0514cddd968d2ea1940bc6a911267e848d0a85d8
ampetr89 <anna.petrone@mapbox.com>
2018-08-16 18:19:13 -0400

    set up express server. began events page

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+* fix the slides.js
+* switch to ejs and make a header template
+* since paypal button is a `form`, should be able to apply an onSubmit() action to capure some info about the payment
---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+// const MobileDetect = require('mobile-detect'),
+
+const app = express();
+
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+  console.log('root');
+  // todo: check if it's a mobile device or desktop
+  // md = new MobileDetect(req.headers['user-agent']);
+  //res.render('index');
+  res.sendFile(__dirname+'/views/index.html');
+});
+
+
+
+app.get('/events', (req, res) => {
+  console.log('root');
+  // todo: check if it's a mobile device or desktop
+  // md = new MobileDetect(req.headers['user-agent']);
+  //res.render('index');
+  res.sendFile(__dirname+'/views/events.html');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on http://localhost:3000');
+  // open('http://localhost:' + config.port);
+});
